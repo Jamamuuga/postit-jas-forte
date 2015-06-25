33794dffb373cfba02b19d38f027d95f3af5eb76
Jason Cady <jamamuuga@gmail.com>
2015-06-25 12:34:28 -0700

    Rest of Comments Creation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :comment, :foreign_key :comment_id
-  belongs_to :user, :foreign_key :user_id
+  belongs_to :post
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527035929) do
+ActiveRecord::Schema.define(version: 20150604185834) do
+
+  create_table "comments", force: true do |t|
+    t.text    "body"
+    t.integer "post_id"
+    t.integer "user_id"
+  end
 
   create_table "posts", force: true do |t|
     t.string  "url"
