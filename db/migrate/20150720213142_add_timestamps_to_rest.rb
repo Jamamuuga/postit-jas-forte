f16540bc238f1b8e46a08c5f3f634e759c4f3081
Jason Cady <jamamuuga@gmail.com>
2015-07-20 16:26:29 -0700

    Removed null: option from add_timestamps so future rails versions aren't stuck with null: true.

--- db/migrate/20150720213142_add_timestamps_to_rest.rb
+++ db/migrate/20150720213142_add_timestamps_to_rest.rb
@@ -1,9 +1,9 @@
 class AddTimestampsToRest < ActiveRecord::Migration
   def change
     # NOTE: null: false gives an error when adding timestamps to an existing table.
-    add_timestamps :categories, null: true
-    add_timestamps :comments, null: true
-    add_timestamps :posts, null: true
-    add_timestamps :users, null: true
+    add_timestamps :categories
+    add_timestamps :comments
+    add_timestamps :posts
+    add_timestamps :users
   end
 end
