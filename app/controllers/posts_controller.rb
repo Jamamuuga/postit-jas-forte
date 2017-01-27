ff69b96eb9e4fd6ab23e0450615bbf1139174dd0
Jason Cady <jamamuuga@gmail.com>
2017-01-27 22:15:23 +0000

    Added initial functionality to posts update action. Note: redirect is using symbol instead of string. will update other actions later for consistency.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,14 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated"
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   private
