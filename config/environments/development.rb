2b5df4b38d59e7715dc940d3b29ffada87d6ca62
Jason Cady <jamamuuga@gmail.com>
2017-03-09 20:02:03 +0000

    Fixed issue of Foreman not outputting rails server messages until exit.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 PostitTemplate::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # This fixes issue of Foreman not outputting rails server messages until exit.
+  $stdout.sync = true
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
